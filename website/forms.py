1c8e12022a25f22ec2b9a6ce028a8153cf0f6af2
Ralph Jones <ralphhjones@gmail.com>
2023-10-02 20:10:47 +0300

    Final project

--- website/forms.py
+++ website/forms.py
@@ -1,18 +1,20 @@
-from typing import Any
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+from .models import Customer
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Email address'}))
-    first_name = forms.CharField(label="", max_length=35, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(label="", max_length=35, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Last Name'}))
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
-    def __init__(self, *args: Any, **kwargs: Any):
+
+    def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
@@ -29,3 +31,18 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+#Add customer to table
+class AddCustomerForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "First Name", "class": "form-control"}), label="")
+    last_name = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "Last Name", "class": "form-control"}), label="")
+    email = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "Email", "class": "form-control"}), label="")
+    phone = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "Phone", "class": "form-control"}), label="")
+    address = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "Address", "class": "form-control"}), label="")
+    city = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "City", "class": "form-control"}), label="")
+    state = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "State", "class": "form-control"}), label="")
+    zipcode = forms.CharField(required=True, widget=forms.widgets.TimeInput(attrs={"placeholder": "ZipCode", "class": "form-control"}), label="")
+
+    class Meta:
+        model=Customer
+        exclude = ("user", )
--- website/views.py
+++ website/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddCustomerForm
+from .models import Customer
 
 # Create your views here.
 def home(request):
+    records = Customer.objects.all()
+
+
+
     #Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +26,7 @@ def home(request):
             return redirect("home")
     else:
         # messages.success(request, "Numb skull..." )
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
@@ -28,4 +34,68 @@ def logout_user(request):
     return redirect("home")
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # lookup record
+        customer = get_object_or_404(Customer, id=pk)
+        return render(request, 'customer.html', {'customer': customer})
+    else:
+        messages.success(request, "You need to log in first.")
+        return redirect('home')
+    
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        print (f"Deleting {pk}")
+        cus = Customer.objects.get(id=pk)
+        cus.delete()
+        messages.success(request, "Customer has been deleted.")
+        print ("Done deleting")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in.")
+        return redirect('home')
+        
+def add_customer(request):
+    form = AddCustomerForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_customer = form.save()
+                messages.success(request, "Customer added.")
+                return redirect('home')
+        return render(request, 'add_customer.html', {'form': form})
+    else:
+        messages.success(request, "You need to be logged in.")
+        return redirect('home')
+
+def update_customer(request, pk):
+    if request.user.is_authenticated:
+        print (f"Updating {pk}")
+        cus = Customer.objects.get(id=pk)
+        form = AddCustomerForm(request.POST or None, instance=cus)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Customer has been updated.")
+            print ("Done updating")
+            return redirect('home')
+        return render(request, 'update_customer.html', {'form': form})
+    else:
+        messages.success(request, "You need to be logged in.")
+        return redirect('home')
